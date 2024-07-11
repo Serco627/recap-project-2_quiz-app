4f27525d5f166df5e218d6346d49b9821478e359
Sercan Sebil <sercansebil95@googlemail.com>
2024-07-11 16:02:49 +0200

    minor changes

---
+++ profile.js
@@ -0,0 +1,16 @@
+const darkModeToggle = document.querySelector("#switch-darkmode");
+console.log(darkModeToggle);
+
+// darkModeToggle.addEventListener("click", () => {
+//   if (darkModeToggle.checked) {
+//     document.body.classList.add("profile__settings-darkmode-label:after");
+//   } else {
+//     document.body.classList.add("profile__settings-darkmode-label:before");
+//   }
+// });
+
+// function darkmode() {
+//   document.body.classList.toggle("dark-mode");
+// }
+
+// darkModeToggle.addEventListener("click", darkmode);
